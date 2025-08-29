b010bd481c64173a3ce0cef2bbdf0df3ef0b4d01
Gurvinder Singh Mudhar <gsm21102004@gmail.com>
2025-08-29 20:37:34 +0530

    deploying on backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: [process.env.CLIENT_URL || "http://localhost:5173"],
+    origin: "http://localhost:5173",
     credentials: true
 }));
 
@@ -30,9 +30,9 @@ app.use('/api/messages', messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+  app.get("/*path", (req, res) => {
+
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html")); });
 }
 
 server.listen(PORT, () => { 
